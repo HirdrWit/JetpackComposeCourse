080e8b896a4c1b7f8eadd0c0fd544815d3f16831
Rob <robertjhird@gmail.com>
2023-10-08 21:48:35 -0400

    Finished JetNote App Course Section

---
+++ JetNote/app/src/main/java/com/example/jetnote/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.jetnote
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.jetnote.screen.NoteScreen
+import com.example.jetnote.screen.NoteViewModel
+import com.example.jetnote.ui.theme.JetNoteTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetNoteTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    val noteViewModel: NoteViewModel by viewModels()
+                    NotesApp(noteViewModel)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun NotesApp(noteViewModel: NoteViewModel = viewModel()) {
+    val notesList = noteViewModel.getAllNotes()
+    NoteScreen(
+        notes = notesList,
+        onAddNote = {
+            noteViewModel.addNote(it)
+        },
+        onRemoveNote = {
+            noteViewModel.removeNote(it)
+        }
+    )
+}
