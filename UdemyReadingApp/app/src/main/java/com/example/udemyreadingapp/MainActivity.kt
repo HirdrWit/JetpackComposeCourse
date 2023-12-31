4f2f1fd36948abbda0a404abd351466ba3a719d0
Rob <robertjhird@gmail.com>
2023-12-30 19:47:49 -0500

    Section 22, 214-218 Splash screen to Login screen

--- UdemyReadingApp/app/src/main/java/com/example/udemyreadingapp/MainActivity.kt
+++ UdemyReadingApp/app/src/main/java/com/example/udemyreadingapp/MainActivity.kt
@@ -1,19 +1,19 @@
 package com.example.udemyreadingapp
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.udemyreadingapp.navigation.ReaderNavigation
 import com.example.udemyreadingapp.ui.theme.UdemyReadingAppTheme
-import com.google.firebase.Firebase
-import com.google.firebase.firestore.FirebaseFirestore
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -22,30 +22,31 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             UdemyReadingAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                ReaderApp()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ReaderApp() {
+    Surface(
+        color = MaterialTheme.colorScheme.background, modifier = Modifier
+            .fillMaxSize()
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            ReaderNavigation()
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun DefaultPreview() {
     UdemyReadingAppTheme {
-        Greeting("Android")
+        ReaderApp()
     }
 }
--- UdemyReadingApp/app/src/main/java/com/example/udemyreadingapp/screen/login/ReaderLoginScreen.kt
+++ UdemyReadingApp/app/src/main/java/com/example/udemyreadingapp/screen/login/ReaderLoginScreen.kt
@@ -1,9 +1,10 @@
 package com.example.udemyreadingapp.screen.login
 
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
 
 @Composable
-fun LoginScreen(navController: NavController){
-
+fun LoginScreen(navController: NavController) {
+    Text(text = "Login Screen")
 }
